9e1c7280538cb23bdbb9a78d47c59150b3bc4a4b
sametozeren <sametozrn@gmail.com>
2020-03-31 17:02:04 +0300

    content.vue

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
     <div id="app">
         <Header />
         <Sidebar />
+        <Content />
     </div>
 </template>
 
 <script>
     import Header from './components/Shared/Header'
     import Sidebar from './components/Shared/Sidebar'
+    import Content from './components/Shared/Content'
 
     export default {
         components: {
             Header,
-            Sidebar
+            Sidebar,
+            Content
         }
     }
 </script>
---
+++ src/components/Shared/Content.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class='content container-fluid'>
+        <div class="row">
+            <div class="col-md-6">
+
+            </div>
+            <div class="col-md-6">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+
+    }
+</script>
+
+<style>
+    .content {
+        width: 100%;
+        margin-left: 90px;
+        position: fixed;
+        min-height: 100%;
+        margin-top: 72px;
+        padding-right: 15px;
+        padding-left: 15px;
+        margin-right: auto;
+    }
+</style>
